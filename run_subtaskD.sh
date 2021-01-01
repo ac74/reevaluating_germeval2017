faea940dfa05f0313d5513d03d68121f61d56edc
ac74 <a.corvonato@campus.lmu.de>
2021-01-01 22:09:14 +0000

    update run_subtaskAB

--- run_subtaskD.sh
+++ run_subtaskD.sh
@@ -1,10 +1,83 @@
 #!/bin/bash
 python3 src/run_subtaskD.py \
-    --lang_model="bert-base-german-dbmdz-uncased" \
-    --batch_size=16 \
-    --epochs=1
+    --lang_model="bert-base-german-cased"
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-german-dbmdz-cased"
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-german-dbmdz-uncased"
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-multilingual-cased"
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-multilingual-uncased"
+
+python3 src/run_subtaskD.py \
+    --lang_model="distilbert-base-german-cased"
+
+python3 src/run_subtaskD.py \
+    --lang_model="distilbert-base-multilingual-cased"
+
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-german-cased" \
+    --max_len=512 \
+    --batch_size=16
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-german-dbmdz-cased" \
+    --max_len=512 \
+    --batch_size=16
 
 python3 src/run_subtaskD.py \
     --lang_model="bert-base-german-dbmdz-uncased" \
-    --batch_size=16 \
-    --epochs=1
+    --max_len=512 \
+    --batch_size=16
+
+python3 src/run_subtaskD.py \
+    --lang_model="distilbert-base-german-cased" \
+    --max_len=512 \
+    --batch_size=16
+
+python3 src/run_subtaskD.py \
+    --lang_model="distilbert-base-multilingual-cased" \
+    --max_len=512 \
+    --batch_size=16
+
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-german-cased" \
+    --max_len=512 \
+    --batch_size=8
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-german-dbmdz-cased" \
+    --max_len=512 \
+    --batch_size=8
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-german-dbmdz-uncased" \
+    --max_len=512 \
+    --batch_size=8
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-multilingual-cased" \
+    --max_len=512 \
+    --batch_size=8
+
+python3 src/run_subtaskD.py \
+    --lang_model="bert-base-multilingual-uncased" \
+    --max_len=512 \
+    --batch_size=8
+
+python3 src/run_subtaskD.py \
+    --lang_model="distilbert-base-german-cased" \
+    --max_len=512 \
+    --batch_size=8
+    
+python3 src/run_subtaskD.py \
+    --lang_model="distilbert-base-multilingual-cased" \
+    --max_len=512 \
+    --batch_size=8
